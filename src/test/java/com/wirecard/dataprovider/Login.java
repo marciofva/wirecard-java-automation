dbe034cf5e9155ded99937a0f87f1241ef6e06f0
Marcio Almeida <marcio.almeida@youse.com.br>
2020-05-29 10:34:58 -0300

    improve login credentials

--- src/test/java/com/wirecard/dataprovider/Login.java
+++ src/test/java/com/wirecard/dataprovider/Login.java
@@ -7,7 +7,7 @@ public class Login {
 	public static HashMap<String, String> existingUserSandbox() {
 		HashMap<String, String> map = new HashMap<String, String>();
 		 map.put("email", "marcio.fva87@gmail.com");
-		 map.put("password", "30302vj5");
+		 map.put("password", "1234abcd");
 		return map;
 	}
 }
